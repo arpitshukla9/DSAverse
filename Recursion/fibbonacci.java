3dd5d3ae8dc882fd2905508d0bfd1ccd0109b71f
arpitshukla9 <arpitshukla0901@gmail.com>
2026-05-31 14:38:33 +0530

    Add: Mulitple Recursion Calls FIBBONACCI

--- Recursion/fibbonacci.java
+++ Recursion/fibbonacci.java
@@ -7,7 +7,7 @@ public class fibbonacci {
     }
     static int fib(int n){
         if(n <= 1) return n;
-        int last = fib(n - 1);
+        int last = fib(n - 1); //jb tk ye return nh krega ans Slast pr move nh krega STack Space yhi wait krega
         int Slast = fib(n - 2);
         return last + Slast;
     }
