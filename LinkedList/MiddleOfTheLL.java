e8d00fc5b1fc83494ffddd3b159e40ca88d20e20
arpitshukla9 <arpitshukla0901@gmail.com>
2026-06-25 12:21:54 +0530

    Add : Middle of the Element in LL

---
+++ LinkedList/MiddleOfTheLL.java
@@ -0,0 +1,13 @@
+public class MiddleOfTheLL {
+    
+    public static Node findMiddle(Node head) {
+        Node fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow; // for node
+        // return slow.data; // for data
+    }
+    
+}
