e815b78d2882a988a7a148b556bcb9254bb1b94e
arpitshukla9 <arpitshukla0901@gmail.com>
2026-06-27 12:40:16 +0530

    Add LinkedList - Add1toNum solution

---
+++ LinkedList/Add1toNum.java
@@ -0,0 +1,22 @@
+
+// ITERATIVE + RECURSIVE (dono se kar sakte hai)
+// YE RECURSIVE MEIN HAI
+class Add1toNum {
+    int addHelper(Node temp){
+        if(temp == null) return 1;
+        int carry = addHelper(temp.next);
+        temp.data  += carry;
+        if(temp.data < 10) return 0;
+        temp.data = 0;
+        return 1;
+    }
+    public Node addOne(Node head) {
+        int carry = addHelper(head);
+        if(carry == 1) {
+            Node newNode = new Node(1);
+            newNode.next = head;
+            head = newNode;
+        }
+        return head;
+    }
+}
